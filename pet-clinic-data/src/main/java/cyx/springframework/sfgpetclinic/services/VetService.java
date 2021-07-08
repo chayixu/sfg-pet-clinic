05f60f40d2a5689e2ea235d24954486ed650bd4a
chaoyi <cx681@nyu.edu>
2021-07-08 01:14:42 -0400

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/services/VetService.java
@@ -3,13 +3,6 @@ package cyx.springframework.sfgpetclinic.services;
 
 import cyx.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
