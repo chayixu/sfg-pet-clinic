f1642bdd2ad34846fdb3a0922fe5c32a21336d32
chaoyi <cx681@nyu.edu>
2021-07-09 21:35:59 -0400

    add PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package cyx.springframework.sfgpetclinic.services;
+
+import cyx.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long> {
+}
