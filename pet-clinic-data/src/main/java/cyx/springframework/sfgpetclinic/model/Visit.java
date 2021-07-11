7b734b25939b72db82b5b797fde3a5f3bf183fd0
chaoyi <cx681@nyu.edu>
2021-07-10 23:27:12 -0400

    Create Visit Map Service. closes #46

--- pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/model/Visit.java
@@ -24,4 +24,20 @@ public class Visit extends BaseEntity{
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
 }
---
+++ pet-clinic-data/src/main/java/cyx/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package cyx.springframework.sfgpetclinic.services;
+
+import cyx.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
