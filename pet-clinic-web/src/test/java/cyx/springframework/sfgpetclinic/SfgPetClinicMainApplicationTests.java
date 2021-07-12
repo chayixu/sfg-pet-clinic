95d4c7a237db640cde124318163250ca9b524908
chaoyi <cx681@nyu.edu>
2021-07-12 13:24:38 -0400

    Upgrade to JUnit 5 and fix a bug of @Builder. closes #4

--- pet-clinic-web/src/test/java/cyx/springframework/sfgpetclinic/SfgPetClinicMainApplicationTests.java
+++ pet-clinic-web/src/test/java/cyx/springframework/sfgpetclinic/SfgPetClinicMainApplicationTests.java
@@ -1,11 +1,13 @@
 package cyx.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicMainApplicationTests {
-
     @Test
     void contextLoads() {
     }
